5ef6b1ea78593c5da99f37c9ff420b777c282c1b
Nethma Kalpani <Kalpani4lk@gmail.com>
2023-12-03 13:29:27 -0500

    Created User model

--- Infinitrax/models.py
+++ Infinitrax/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class User():
+    username = models.CharField(max_length=100, primary_key=True)
+    password = models.CharField(max_length=100)
 
 class Category(models.Model):
     categories = models.CharField(max_length=255)
