573f766b4c5c7592f9aefbdf15849598d0ad4841
Nethma Kalpani <Kalpani4lk@gmail.com>
2023-12-03 13:28:07 -0500

    Configured settings

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'Infinitrax.apps.InfinitraxConfig'
+    'Infinitrax.apps.InfinitraxConfig',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,8 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'infinitrax',
     }
 }
 
@@ -88,6 +89,7 @@ import dj_database_url
 
 DATABASES['default'] = dj_database_url.parse('mysql://root@localhost/infinitrax')
 
+# AUTH_USER_MODEL = 'Infinitrax.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -107,6 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'knox.auth.TokenAuthentication',
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
