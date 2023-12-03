cca159d450f779fe261cc484e7932d2c87fb0218
Nethma Kalpani <Kalpani4lk@gmail.com>
2023-12-03 13:30:46 -0500

    Created UserSerializer

--- Infinitrax/serializers.py
+++ Infinitrax/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from Infinitrax import models
 from Infinitrax.models import Category
 from Infinitrax.models import Brand
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = ['username', 'password']
+
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+            },
+        }
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
