260c63f2cd7a6186ed53a385669ce96e711ed621
Nethma Kalpani <Kalpani4lk@gmail.com>
2023-12-04 03:23:40 -0500

    Created serializer for attibutes

--- Infinitrax/serializers.py
+++ Infinitrax/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from Infinitrax import models
 from Infinitrax.models import Category
 from Infinitrax.models import Brand
+from Infinitrax.models import Attribute
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,4 +23,9 @@ class CategorySerializer(serializers.ModelSerializer):
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
+        fields = '__all__'
+
+class AttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attribute
         fields = '__all__'
